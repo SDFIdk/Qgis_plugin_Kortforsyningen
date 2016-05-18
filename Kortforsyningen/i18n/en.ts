0b31c4d3bd35d057dbdefd01db060391328aad98
mie winstrup <mie@septima.dk>
2016-05-18 23:52:11 +0200

    Translation of English text to Danish

---
+++ Kortforsyningen/i18n/en.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>Kortforsyningen</name>
+    <message>
+        <location filename="kortforsyningen.py" line="288"/>
+        <source>Kortforsyningen</source>
+        <translation>Kortforsyningen</translation>
+    </message>
+    <message>
+        <location filename="kortforsyningen.py" line="102"/>
+        <source>No contact to Kortforsyning</source>
+        <translation>Ingen kontakt til Kortforsyningen</translation>
+    </message>
+    <message>
+        <location filename="kortforsyningen.py" line="134"/>
+        <source>No contact to Kortforsyningen</source>
+        <translation>Ingen kontakt til Kortforsyningen</translation>
+    </message>
+    <message>
+        <location filename="kortforsyningen.py" line="324"/>
+        <source>Settings</source>
+        <translation>Indstillinger</translation>
+    </message>
+    <message>
+        <location filename="kortforsyningen.py" line="270"/>
+        <source>Error</source>
+        <translation>Fejl</translation>
+    </message>
+    <message>
+        <location filename="kortforsyningen.py" line="270"/>
+        <source>Could not load the layer. Is username and password correct?</source>
+        <translation>Kunne ikke indlæse laget. Er brugernavn og password korrekte?</translation>
+    </message>
+    <message>
+        <location filename="kortforsyningen.py" line="333"/>
+        <source>About the plugin</source>
+        <translation>Om pluginet</translation>
+    </message>
+    <message>
+        <location filename="kortforsyningen.py" line="242"/>
+        <source>Please, fill out username and password</source>
+        <translation>Udfyld venligst brugernavn og kodeord.</translation>
+    </message>
+</context>
+</TS>
