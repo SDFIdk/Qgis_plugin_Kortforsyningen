deb2d1c0a25efe06842794f27ad9f9b182027fb9
klavs <klavs@septima.dk>
2016-11-29 10:28:31 +0100

    Settings opdateret med lokal .qlr-fil

--- Kortforsyningen/kortforsyningen_settings.py
+++ Kortforsyningen/kortforsyningen_settings.py
@@ -35,6 +35,8 @@ class KFSettings(SettingManager):
         SettingManager.__init__(self, 'Kortforsyningen')
         self.addSetting('username', 'string', 'global', '')
         self.addSetting('password', 'string', 'global', '')
+        self.addSetting('use_custom_qlr_file', 'bool', 'global', False)
+        self.addSetting('custom_qlr_file', 'string', 'global', '')
         self.addSetting('remember_settings', 'bool', 'global', False)
         
     def is_set(self):
@@ -45,5 +47,31 @@ class KFSettings(SettingManager):
 class KFSettingsDialog(QtGui.QDialog, FORM_CLASS, SettingDialog):
     def __init__(self, settings):
         QtGui.QDialog.__init__(self)
+        #self.settings = settings
         self.setupUi(self)
         SettingDialog.__init__(self, settings)
+        if self.use_custom_qlr_file.isChecked():
+            self.browseLocalFileButton.setEnabled(True)
+        else:
+            self.browseLocalFileButton.setEnabled(False)
+
+        self.browseLocalFileButton.clicked.connect(self.browseLocalFile)
+        self.use_custom_qlr_file.clicked.connect(self.useLocalChanged)
+        
+    def browseLocalFile(self):
+        file = QtGui.QFileDialog.getOpenFileName(
+            self,
+            "Lokal qlr",
+            self.custom_qlr_file.text(),
+            "Qlr (*.qlr)"
+        )
+        if file:
+            #self.settings.set_value('custom_qlr_file', file)
+            self.custom_qlr_file.setText(file)
+
+    def useLocalChanged(self, checked):
+        #if self.use_custom_qlr_file.isChecked():
+        self.browseLocalFileButton.setEnabled(checked)
+        #else:
+        #    self.browseLocalFileButton.setEnabled(False)
+
