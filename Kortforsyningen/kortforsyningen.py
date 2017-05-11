b661bb7edecd235016cba803ffe7b38f03e41321
klavs <klavs@septima.dk>
2017-05-11 12:33:31 +0200

    Testet wfs

--- Kortforsyningen/kortforsyningen.py
+++ Kortforsyningen/kortforsyningen.py
@@ -61,8 +61,9 @@ from config import Config
 
 #Develop
 #CONFIG_FILE_URL = 'http://labs.septima.dk/qgis-kf-knap/kortforsyning_data.qlr'
+#CONFIG_FILE_URL = 'http://labs.septima.dk/qgis-kf-knap/kortforsyning_data_SDFE.qlr'
+##test version: 'http://labs.septima.dk/qgis-kf-knap/kortforsyning_data_inkl_restricteddata.qlr'
 CONFIG_FILE_URL = 'http://apps2.kortforsyningen.dk/qgis_knap_config/Kortforsyningen/qgis_plugin.qlr'
-    ##test version: 'http://labs.septima.dk/qgis-kf-knap/kortforsyning_data_inkl_restricteddata.qlr'
 
 ABOUT_FILE_URL = 'http://apps2.kortforsyningen.dk/qgis_knap_config/Kortforsyningen/about.html'
 FILE_MAX_AGE = datetime.timedelta(hours=12)
